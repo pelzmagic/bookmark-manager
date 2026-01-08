1ec8280e49ba9388d935f3af547eee7598e478ec
pelzmagic <akinloyepelz@gmail.com>
2026-01-08 21:52:14 +0100

    add styling to signup page

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,27 +1,55 @@
 export default function SignUp() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-neutral-100">
-      <div className="bg-neutral-0 flex max-w-85.75 flex-col gap-8 rounded-xl px-5 py-8">
-        <div>
+      <div className="flex max-w-85.75 flex-col gap-8 rounded-xl bg-white px-5 py-8">
+        <div className="flex items-center gap-2">
           <img src="/Bookmark.png" alt="bookmark icon" className="h-8 w-8" />
-          <p>Bookmark Manager</p>
+          <p className="font-roboto text-xl leading-[100%] font-bold text-neutral-900">
+            Bookmark Manager
+          </p>
         </div>
 
-        <div>
-          <h1>Log in to your account</h1>
-          <p>Welcome back! Please enter your details</p>
+        <div className="flex flex-col gap-1.5">
+          <h1 className="font-manrope lelading-[140%] text-2xl font-bold text-neutral-900">
+            Log in to your account
+          </h1>
+          <p className="font-manrope text-sm leading-[150%] font-medium text-neutral-800">
+            Welcome back! Please enter your details
+          </p>
         </div>
 
-        <form>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input type="text" />
+        <form className="flex flex-col gap-4">
+          <div className="flex flex-col gap-1.5">
+            <label
+              htmlFor="email"
+              className="font-manrope text-sm leading-[140%] font-semibold text-neutral-900"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="rounded-lg border border-neutral-500 p-3 shadow-xs outline-0"
+            />
           </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input type="text" />
+          <div className="flex flex-col gap-1.5">
+            <label
+              htmlFor="password"
+              className="font-manrope text-sm leading-[140%] font-semibold text-neutral-900"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="rounded-lg border border-neutral-500 p-3 shadow-xs outline-0"
+            />
           </div>
-          <button>Log in</button>
+          <button className="font-manrope rounded-lg bg-teal-700 px-4 py-3 text-base leading-[140%] font-semibold text-white">
+            Log in
+          </button>
         </form>
 
         <div>
