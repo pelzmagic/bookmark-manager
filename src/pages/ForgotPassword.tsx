0adb13891d58385b1698971ab5aaa70a100be846
pelzmagic <akinloyepelz@gmail.com>
2026-01-10 16:44:03 +0100

    feat:add complete styling to forgot password page

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -14,9 +14,34 @@ export default function ForgotPassword() {
             Forgot your password?
           </h1>
           <p className="font-manrope text-sm leading-[150%] font-medium text-neutral-800">
-            Enter your
+            Enter your email address below and we'll send you a link to reset
+            your password.
           </p>
         </div>
+
+        <form className="flex flex-col gap-4">
+          <div className="flex flex-col gap-1.5">
+            <label
+              htmlFor="email"
+              className="font-manrope text-sm leading-[140%] font-semibold text-neutral-900"
+            >
+              Email <span className="text-neutral-900">*</span>
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="rounded-lg border border-neutral-500 p-3 shadow-xs outline-0"
+            />
+          </div>
+          <button className="font-manrope rounded-lg bg-teal-700 px-4 py-3 text-base leading-[140%] font-semibold text-white">
+            Send reset Link
+          </button>
+        </form>
+
+        <p className="font-manrope text-center text-sm leading-[140%] font-semibold text-neutral-900">
+          Back to login
+        </p>
       </div>
     </div>
   );
