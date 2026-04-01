aa5b789aa578bc2d8652f822f5ecbd21d010f86e
pelzmagic <akinloyepelz@gmail.com>
2026-04-02 00:35:12 +0100

    feat: add tag

--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { Checkbox } from "@/components/ui/checkbox";
+
 export default function Sidebar() {
   return (
     <aside className="border-light-300 row-start-1 -row-end-1 flex flex-col gap-4 border-r">
@@ -33,7 +35,15 @@ export default function Sidebar() {
         <p className="font-manrope px-3 pb-1 text-xs leading-[140%] font-bold text-[#4D4D4D]">
           TAGS
         </p>
-        <div></div>
+        <div className="flex items-center justify-between px-3 py-2">
+          <div className="flex items-center gap-2">
+            <Checkbox />
+            <p>AI</p>
+          </div>
+          <div className="bg-light-300 font-manrope text-light-800 rounded-full px-2 py-0.5 text-xs leading-[140%] font-medium">
+            1
+          </div>
+        </div>
       </div>
     </aside>
   );
