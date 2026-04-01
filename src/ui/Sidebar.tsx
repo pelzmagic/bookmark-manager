6578f86a361f160ada8f97af2cec8aeb38ffa647
pelzmagic <akinloyepelz@gmail.com>
2026-04-01 18:40:46 +0100

    feat:add bookmark logo to sidebar

--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -1,7 +1,12 @@
 export default function Sidebar() {
   return (
-    <aside className="border-light-300 row-start-1 -row-end-1 border-r">
-      Sidebar
+    <aside className="border-light-300 row-start-1 -row-end-1 flex flex-col gap-4 border-r">
+      <div className="flex items-center gap-5 px-5 pt-5 pb-2.5">
+        <img src="/Bookmark.png" alt="logo icon" className="h-8 w-8" />
+        <p className="font-roboto text-light-900 text-[20px] leading-[100%] font-bold tracking-[-1%]">
+          Bookmark Manager
+        </p>
+      </div>
     </aside>
   );
 }
