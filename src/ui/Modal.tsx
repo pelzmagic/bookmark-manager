ac2a3a78eb042c647710b6fd0d8797108f16b40b
pelzmagic <akinloyepelz@gmail.com>
2026-04-09 23:50:23 +0100

    feat:add function to button in compound component

--- src/ui/Modal.tsx
+++ src/ui/Modal.tsx
@@ -1,6 +1,37 @@
 import { createContext } from "react";
+import { useState } from "react";
+import { createPortal } from "react-dom";
+import { useContext } from "react";
+import { cloneElement } from "react";
 
 const ModalContext = createContext();
-export default function Modal({ children }: { children: React.ReactNode }) {
-  return <h1>How are you?</h1>;
+
+function Modal({ children }: { children: React.ReactNode }) {
+  const [openName, setOpenName] = useState("");
+
+  const close = () => setOpenName("");
+  const open = setOpenName;
 }
+
+function Open({ children, opens: openWindowName }) {
+  const { open } = useContext(ModalContext);
+
+  return cloneElement(children, { onClick: () => open(openWindowName) });
+}
+
+function Window({ children, name }) {
+  return createPortal(
+    <div className="fixed inset-0 z-10 flex items-center justify-center bg-[#131313]/70 backdrop-blur-sm transition-all">
+      <div className="relative max-w-142.5 rounded-2xl bg-white p-8">
+        <button className="bg-light-400 absolute top-5 right-5 rounded-lg p-1.5">
+          <img src="/x-close.png" alt="close icon" className="h-5 w-5" />
+        </button>
+      </div>
+    </div>,
+    document.body,
+  );
+}
+
+Modal.Open = Open;
+Modal.Window = Window;
+export default Modal;
