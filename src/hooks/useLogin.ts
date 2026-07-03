0727bee6351ce3217730bab653ebacf050b060dd
pelzmagic <akinloyepelz@gmail.com>
2026-07-03 14:20:15 +0100

    fix:update log in hook

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -2,13 +2,16 @@ import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { useNavigate } from "react-router-dom";
 import { login as loginApi } from "@/services/auth";
+import { useQueryClient } from "@tanstack/react-query";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isPending: isLoggingIn } = useMutation({
     mutationFn: loginApi,
-    onSuccess: () => {
+    onSuccess: (user) => {
+      queryClient.setQueryData(["user"], user);
       toast.success("Sign in successful");
       navigate("/dashboard");
     },
